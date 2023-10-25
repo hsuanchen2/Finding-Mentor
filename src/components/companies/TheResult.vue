1e43b3709402fb5087d3c78d4a7802aa3ed84d27
hsuanchen <hsuanchen321@gmail.com>
2023-10-25 23:54:45 +0800

    add hero animation

--- src/components/companies/TheResult.vue
+++ src/components/companies/TheResult.vue
@@ -1,70 +1,102 @@
 <template>
+  <div class="wrapper">
     <div class="client-result-wrapper">
-        <div class="client-result-copy">
-            <h2>With Exceptional Outcomes!</h2>
-            <p>Our clients have received job offers from incredible organizations</p>
+      <div class="client-result-copy">
+        <h2>With Exceptional Outcomes!</h2>
+        <p>
+          Our clients have received job offers from incredible organizations
+        </p>
+      </div>
+      <section class="client-results container">
+        <div class="row g-2">
+          <div class="col col-lg-2 col-md-4 col-sm-6 col-6">
+            <img
+              src="../../../public/company-logo/google.png"
+              alt="google"
+              loading="lazy"
+            />
+          </div>
+          <div class="col col-lg-2 col-md-4 col-sm-6 col-6">
+            <img
+              src="../../../public/company-logo/facebook.png"
+              alt="facebook"
+              loading="lazy"
+            />
+          </div>
+          <div class="col col-lg-2 col-md-4 col-sm-6 col-6">
+            <img
+              src="../../../public/company-logo/amazon.png"
+              alt="amazon"
+              loading="lazy"
+            />
+          </div>
+          <div class="col col-lg-2 col-md-4 col-sm-6 col-6">
+            <img
+              src="../../../public/company-logo/bloomberg.png"
+              alt="bloomberg"
+              loading="lazy"
+            />
+          </div>
+          <div class="col col-lg-2 col-md-4 col-sm-6 col-6">
+            <img
+              src="../../../public/company-logo/microsoft.png"
+              alt="microsoft"
+              loading="lazy"
+            />
+          </div>
+          <div class="col col-lg-2 col-md-4 col-sm-6 col-6">
+            <img
+              src="../../../public/company-logo/twosigma.png"
+              alt="twosigma"
+              loading="lazy"
+            />
+          </div>
         </div>
-        <section class="client-results container">
-            <div class="row">
-                <div class="col col-sm-6 col-lg-2 col-md-4">
-                    <img src="../../../public/company-logo/google.png" alt="google" loading="lazy">
-                </div>
-                <div class="col col-sm-6 col-lg-2 col-md-4">
-                    <img src="../../../public/company-logo/facebook.png" alt="facebook" loading="lazy">
-                </div>
-                <div class="col col-sm-6 col-lg-2 col-md-4">
-                    <img src="../../../public/company-logo/amazon.png" alt="amazon" loading="lazy">
-                </div>
-                <div class="col col-sm-6 col-lg-2 col-md-4">
-                    <img src="../../../public/company-logo/bloomberg.png" alt="bloomberg" loading="lazy">
-                </div>
-                <div class="col col-sm-6 col-lg-2 col-md-4">
-                    <img src="../../../public/company-logo/microsoft.png" alt="microsoft" loading="lazy">
-                </div>
-                <div class="col col-sm-6 col-lg-2 col-md-4">
-                    <img src="../../../public/company-logo/twosigma.png" alt="twosigma" loading="lazy">
-                </div>
-            </div>
-        </section>
+      </section>
     </div>
+  </div>
 </template>
 <script></script>
 <style lang="scss" scoped>
+.wrapper {
+    background-color: white;
+}
 .client-result-wrapper {
-    max-width: 1200px;
-    margin: 100px auto;
+  max-width: 1200px;
+  margin: 100px auto;
+  text-align: center;
+  overflow: hidden;
+  .client-result-copy {
     text-align: center;
-
-    .client-result-copy {
-        text-align: center;
-        margin-bottom: 40px;
-        h2 {
-            font-weight: 700;
-            letter-spacing: 0.05em;
-            font-size: 2.25rem;
-            color: #2c3e50;
-        }
-        p {
-            color: #425466;
-            font-size: 1.1rem;
-        }
+    margin-bottom: 40px;
+    margin-top: 50px;
+    h2 {
+      font-weight: 700;
+      letter-spacing: 0.05em;
+      font-size: 2.25rem;
+      color: #2c3e50;
     }
+    p {
+      color: #425466;
+      font-size: 1.1rem;
+    }
+  }
 }
 
 .container {
-    .row {
-        .col {
-            text-align: center;
-            margin-bottom: 25px;
-        }
-
-        align-items: start;
-
-        img {
-            width: 70%;
-            object-fit: contain;
-            object-position: center;
-        }
+  .row {
+    .col {
+      text-align: center;
+      margin-bottom: 35px;
     }
+
+    align-items: start;
+
+    img {
+      width: 70%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
 }
 </style>
