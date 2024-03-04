72f706f77372334fe1f04ec86f00868b59e5397a
hsuanchen2 <hsuanchen321@gmail.com>
2024-03-04 18:01:55 +0800

    finish chatbox layout

--- src/pages/chat/ChatBox.vue
+++ src/pages/chat/ChatBox.vue
@@ -1,16 +1,82 @@
 <template>
   <section class="wrapper row">
-    <aside class="chat-list col col-3">
+    <aside class="chat-list col col-xl-3 col-6 d-none d-xl-block">
       <h4 class="title">Chats</h4>
       <input type="text" placeholder="Search user" class="search-input" />
       <div class="chat-buttons-container">
-        <button class="chat-button"></button>
-        <button class="chat-button"></button>
-        <button class="chat-button"></button>
-        <button class="chat-button"></button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
+        <button class="chat-button">
+          <img src="@/../public/user-img/test2.jpg" class="chat-button-img" alt="">
+          <div class="user-info">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+          </div>
+        </button>
       </div>
     </aside>
-    <div class="chat-box col col-7">
+    <div class="chat-box col col-xl-6">
       <section>
         <header class="chat-header">
           <img class="user-avatar" src="@/../public/user-img/test2.jpg" alt="" />
@@ -62,27 +128,73 @@
               </div>
             </div>
           </div>
+          <div class="chat outgoing">
+            <div class="details">
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <div class="time-stamp">
+                <span>10:30 AM</span>
+              </div>
+            </div>
+          </div>
         </div>
         <form action="#" class="text-input-form">
-          <input type="text" placeholder="Type a message" />
+          <input class="input-area" type="text" placeholder="Type a message" />
+          <button type="submit" class="submit-button"><i class="fa-regular fa-paper-plane"></i></button>
         </form>
       </section>
     </div>
-    <div class="col col-2 suggestions">
-      <section class="">
-        <h4>Suggestions</h4>
-      </section>
+    <div class="col col-xl-3 contact-info">
+      <header class="">
+        <h4>Contact Info</h4>
+      </header>
+      <main class="contact-user-info">
+        <div class="contact-user-card">
+          <img src="@/../public/user-img/test2.jpg" class="user-avatar" alt="">
+          <div class="contact-title-wrapper">
+            <h3>Patrick Shyu</h3>
+            <h4>ex Google ex Facebook tech lead</h4>
+            <p>&#x2B50 4.4 (32)</p>
+          </div>
+        </div>
+        <div class="contact-user-details">
+          <div class="email">
+            <h4>Email</h4>
+            <p>techleadhd@gmail.com</p>
+          </div>
+          <div class="location">
+            <h4>Location</h4>
+            <p>United States</p>
+          </div>
+          <div class="email">
+            <h4>Charge</h4>
+            <p>$45 / hour</p>
+          </div>
+          <div class="location">
+            <h4>Expertise</h4>
+            <p>Software engineer</p>
+          </div>
+          <div class="skills">
+            <h4>Skills</h4>
+            <div class="skills-tags">
+              <SkillTag>Vue</SkillTag>
+              <SkillTag>Nodejs</SkillTag> 
+            </div>
+          </div>
+        </div>
+
+      </main>
     </div>
   </section>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import SkillTag from "@/components/ui/SkillTag.vue";
+</script>
 <style lang="scss" scoped>
 .wrapper {
   max-width: 1200px;
   margin: 50px auto;
   border-radius: 10px;
   box-shadow: $light-card-shadow;
-  height: 550px;
   box-sizing: content-box;
 }
 
@@ -111,26 +223,15 @@
 .chat-buttons-container {
   padding-top: 5px;
   padding-right: 5px;
-  height: 440px;
+  height: 460px;
   overflow-y: auto;
   margin: 10px 0 15px 0;
-  padding-left: 10px;
+  padding-left: 5px;
 
   h4 {
     color: $main-text-color;
   }
 
-  .chat-button {
-    background-color: white;
-    margin-bottom: 10px;
-    width: 100%;
-    border: none;
-    transition: 0.3s;
-
-    &:hover {
-      background-color: rgb(184, 184, 184);
-    }
-  }
 }
 
 .chat-button {
@@ -138,6 +239,44 @@
   border-radius: 5px;
   list-style-type: none;
   height: 45px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  width: 100%;
+  border: none;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: rgb(184, 184, 184);
+  }
+
+  img {
+    width: 35px;
+    height: 35px;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+
+  .user-info {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    margin-left: 10px;
+
+    h3 {
+      font-size: 14px;
+      color: $main-text-color;
+      margin-bottom: 0;
+    }
+
+    h4 {
+      font-size: 12px;
+      color: $minor-text-color;
+      margin-bottom: 0;
+    }
+  }
 }
 
 .chat-box {
@@ -148,7 +287,9 @@
 
 .chat-area {
   padding: 15px 20px;
-  height: 430px;
+  height: 450px;
+  overflow: auto;
+
 
   .incoming {
     .time-stamp {
@@ -226,7 +367,7 @@
 
 .chat-header {
   display: flex;
-  padding-top: 15px;
+  padding-top: 10px;
   padding-left: 15px;
   align-items: center;
   padding-bottom: 10px;
@@ -253,21 +394,132 @@
       font-weight: 400;
       font-style: italic;
       color: $minor-text-color;
+      margin-bottom: 0;
     }
   }
 }
 
-.suggestions {
+.contact-info {
   padding-top: 20px;
 
-  h4 {
+  header h4 {
     text-align: center;
+  }
+
+  main.contact-user-info {
+    .skills-tags {
+      display: flex;
+      overflow-x: auto;
+      gap: 5px;
+      margin-top: 7px;
+    }
+
+    .contact-user-details {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      padding-left: 20px;
+
+      h4 {
+        font-size: 14px;
+        margin-bottom: 0;
+        color: $main-text-color;
+      }
+
+      p {
+        font-size: 15px;
+        margin-bottom: 0;
+        word-wrap: break-word;
+        overflow-wrap: break-word;
+        color: $minor-text-color;
+      }
+    }
+
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+
+    .contact-user-card {
+      margin-top: 20px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 10px;
+
+      .contact-title-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 5px;
+        margin-bottom: 25px;
+
+        h3 {
+          font-size: 18px;
+          color: $main-text-color;
+          margin-bottom: 0;
+        }
+
+        h4 {
+          font-size: 14px;
+          margin-bottom: 0;
+          color: $minor-text-color;
+          font-weight: 400;
+          font-style: italic;
+        }
+
+        p {
+          font-size: 14px;
+          color: $minor-text-color;
+          margin-bottom: 0;
+        }
+      }
+    }
   }
 }
 
 .text-input-form {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   border-top: 1px solid lightgray;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding: 10px 20px;
+
+  .input-area {
+    height: 40px;
+    width: calc(100% - 58px);
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(204, 204, 204);
+    border-image: initial;
+    padding: 0px 13px;
+    border-radius: 5px;
+    outline: none;
+  }
+
+  .submit-button {
+    width: 55px;
+    height: 40px;
+    font-size: 19px;
+    cursor: pointer;
+    color: rgb(255, 255, 255);
+    border-width: initial;
+    border-style: none;
+    border-color: initial;
+    border-image: initial;
+    outline: none;
+    border-radius: 5px 5px 5px 5px;
+    background-color: $main-button-color;
+    margin-left: 10px;
+    transition: .3s;
+
+    &:hover {
+      background-color: lighten($main-button-color, 10%);
+    }
+  }
 }
 </style>
